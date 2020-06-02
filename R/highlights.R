0e188fd1f46ce67d4d096446fff6e8dc1c5df1f6
Akira Hayashi <smxshxishxad@me.com>
2020-06-02 15:22:17 +0900

    :up: Abstruct code

--- R/highlights.R
+++ R/highlights.R
@@ -1,15 +1,32 @@
+#' @param string String to wrap with html tag
+#' @param color Color to use for background
+#' @param tag Tag to use for wrapping \code{string}
+wrap_html_bgcolor <- function(string, color, tag) {
+  paste0("<", " style=\"background-color:", color, "\">",
+         string,
+         "</", tag, ">")
+}
+
+#' Generate html code with highlighted string
+#'
+#' Use this function with \code{params} function of Rmarkdown.
+#' @inheritParams wrap_html_bgcolor
+#' @export
 highlight <- function(string, color = "skyblue") {
   if (params$draft) {
-    return(paste0("<span style=\"background-color:", color, "\">",
-                  string, "</span>"))
+    return(wrap_html_bgcolor(string, color = color, tag = "span"))
   }
   force(string)
 }
 
-checkme <- function(string) {
+#' Generate html code with highlighted string with line break
+#'
+#' Use this function with \code{params} function of Rmarkdown.
+#' @inheritparams wrap_html_bgcolor
+#' @export
+checkme <- function(string, color = yellow) {
   if (params$draft) {
-    return(paste0("<div style=\"background-color:yellow\">- \u25A2 ",
-                  string, "</div>"))
+    return(wrap_html_bgcolor(paste0("- \u25A2", string), color = color, tag = "div"))
   }
   force(string)
 }
